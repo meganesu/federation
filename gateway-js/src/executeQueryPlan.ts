f8e5a09776b62bba0b0290cf458303fc71621897
Trevor Scheer <trevor.scheer@gmail.com>
2022-11-09 15:11:04 -0800

    missing optional

--- gateway-js/src/executeQueryPlan.ts
+++ gateway-js/src/executeQueryPlan.ts
@@ -435,7 +435,7 @@ async function executeFetch(
 
       if (!response.extensions?.ftv1) {
         context.requestContext.metrics.nonFtv1Errors = [
-            ...(context.requestContext.metrics.nonFtv1Errors ?? []),
+            ...(context.requestContext.metrics?.nonFtv1Errors ?? []),
             ...errors,
         ]
       }
